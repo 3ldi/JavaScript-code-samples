362f7b55804a54abf9199889b6c986a1d00ad05e
eldi <66099058+3ldi@users.noreply.github.com>
2023-01-13 18:33:44 -0500

    Create arrayMergin.js

---
+++ arrayMergin.js
@@ -0,0 +1,13 @@
+// given two arrays and an index copy each element of the first array into the second array, in order.
+// Begin inserting elements at index n of the second array.
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+function mergeArr(arr1, arr2, n) {
+
+  let newArr = [];
+  
+  // Split the second array on position "n" and concat arr1 in between the 2 pieces
+  newArr = arr2.slice(0, n).concat(arr1).concat(arr2.slice(n));
+  
+  return newArr;
+}
