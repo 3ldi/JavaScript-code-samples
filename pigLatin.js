be18b5c98528430861574e6aea87863feca165c7
eldi <66099058+3ldi@users.noreply.github.com>
2023-01-19 22:27:47 -0500

    Updated comments

--- pigLatin.js
+++ pigLatin.js
@@ -1,22 +1,24 @@
-
+// Pig Latin is a way of altering English Words. The rules are as follows:
+// - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+// - If a word begins with a vowel, just add way at the end.
 
 function translatePigLatin(str) {
-  
-let arr = str.split("");
-let myVowels = /[aeuio]/i;
 
-if (arr[0].match(myVowels)){
-// Check if word starts with a vowel 
-	return arr.concat(['way']).join("");
-} else if(!str.match(myVowels)) {
-// Check if word has no vowels
-	return arr.concat(['ay']).join("");
-} else {
-// check if word starts with a consonant or a consonant group
-	for( let i = 1; i < arr.length; i++) {
-		if (arr[i].match(myVowels)) {
-			return (arr.slice(i).concat(arr.slice(0, i), ["ay"]).join(""));
-          }
-      }
-  }
+	let arr = str.split("");
+	let myVowels = /[aeuio]/i;
+
+	if (arr[0].match(myVowels)) {
+		// Check if word starts with a vowel 
+		return arr.concat(['way']).join("");
+	} else if (!str.match(myVowels)) {
+		// Check if word has no vowels
+		return arr.concat(['ay']).join("");
+	} else {
+		// check if word starts with a consonant or a consonant group
+		for (let i = 1; i < arr.length; i++) {
+			if (arr[i].match(myVowels)) {
+				return (arr.slice(i).concat(arr.slice(0, i), ["ay"]).join(""));
+			}
+		}
+	}
 }
